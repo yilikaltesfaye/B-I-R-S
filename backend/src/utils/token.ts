12b03885fb6566b49878ca55c03025a4b9edc230
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-09 17:54:36 +0300

    feat(jwt): create function that generate access token and refresh token

---
+++ backend/src/utils/token.ts
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+export const generateAccessToken = (userId: String) => {
+	return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
+		expiresIn: "15m",
+	});
+};
+export const generateRefreshToken = (userId: String) => {
+	return jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET!, {
+		expiresIn: "7d",
+	});
+};
