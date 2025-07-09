4d75047777a7106f07f337b6a5473c9977920d6e
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-09 17:53:32 +0300

    feat(server): create an instance express server to run

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import AuthRoutes from "./routes/auth.routes";
+
+const app = express();
+
+app.use(cors({ origin: "https:localhost:3000", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", AuthRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}`);
+});
