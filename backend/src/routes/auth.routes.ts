7cb3831f48230ceb4f590246365f92a3715b4641
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-09 17:55:34 +0300

    feat(auth): create routes for signup, login, logout and the works

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+	signup,
+	login,
+	logout,
+	// forgotPassword,
+	// resetPassword,
+	// refreshToken,
+} from "../controllers/auth.controller";
+
+const router = Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+// router.post("/forgot-password", forgotPassword);
+// router.post("/reset-password", resetPassword);
+// router.post("/refresh-token", refreshToken);
+
+export default router;
