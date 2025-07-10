fa2bc2cc90ff1417db9ba52c18b4721246b5ecb9
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-10 16:45:31 +0300

    feat(zod): validate inputs from request with zod

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,16 +1,27 @@
 import { Request, Response } from "express";
 import * as AuthService from "./auth.service";
 import prisma from "../../prisma/client";
+import {
+	PasswordSchema,
+	LoginSchema,
+	SignupSchema,
+} from "../../types/auth.interface";
+import { standardPhone } from "../../utils/standardPhoneNumber";
+import z from "zod";
+
+// signup controller
 
 export const signup = async (req: Request, res: Response) => {
 	try {
-		const { name, email, phone, password, region } = req.body;
+		// const { name, email, phone, password, region } = req.body;
+		const validated = SignupSchema.parse(req.body);
+		const phone = standardPhone(validated.phoneNumber);
 		const { accessToken, refreshToken } = await AuthService.signup(
-			name,
-			email,
+			validated.fullName,
 			phone,
-			password,
-			region
+			validated.password,
+			validated.region,
+			validated.email
 		);
 
 		res.cookie("refreshToken", refreshToken, {
@@ -21,16 +32,23 @@ export const signup = async (req: Request, res: Response) => {
 		});
 		res.json({ accessToken });
 	} catch (error: any) {
+		if (error instanceof z.ZodError) {
+			return res.status(400).json({ errors: error.errors });
+		}
 		res.status(400).json({ error: error.message });
 	}
 };
+
+// login controller
 
 export const login = async (req: Request, res: Response) => {
 	try {
-		const { phone, password } = req.body;
+		// const { phone, password } = req.body;
+		const validated = LoginSchema.parse(req.body);
+		const phone = standardPhone(validated.phoneNumber);
 		const { accessToken, refreshToken } = await AuthService.login(
 			phone,
-			password
+			validated.password
 		);
 
 		res.cookie("refreshToken", refreshToken, {
@@ -41,9 +59,14 @@ export const login = async (req: Request, res: Response) => {
 		});
 		res.json({ accessToken });
 	} catch (error: any) {
+		if (error instanceof z.ZodError) {
+			return res.status(400).json({ errors: error.errors });
+		}
 		res.status(400).json({ error: error.message });
 	}
 };
+
+// logout controller
 
 export const logout = async (_req: Request, res: Response) => {
 	try {
@@ -58,17 +81,19 @@ export const logout = async (_req: Request, res: Response) => {
 	}
 };
 
-export const refreshToken = async (req: Request, res: Response) => {
-	try {
-		const token = req.body?.refreshToken || req.cookies.refreshToken;
+// refrest access token controller
 
-		if (!token) {
+export const refreshAccessToken = async (req: Request, res: Response) => {
+	try {
+		const refreshToken = req.body?.refreshToken || req.cookies.refreshToken;
+
+		if (!refreshToken) {
 			return res.status(401).json({ error: "Refresh token missing" });
 		}
 
 		const isMobileClient = req.headers["user-agent"]?.includes("ReactNative");
 
-		const { accessToken, refreshToken } = AuthService.refreshToken(token);
+		const { accessToken } = AuthService.refreshAccessToken(refreshToken);
 
 		if (isMobileClient) {
 			res.json({ accessToken, refreshToken });
@@ -88,9 +113,46 @@ export const refreshToken = async (req: Request, res: Response) => {
 	}
 };
 
+// refresh cookie refresh token controller
+
+export const refreshCookieToken = async (req: Request, res: Response) => {
+	try {
+		const { userId, userRole } = req.body.userId;
+
+		if (!userId || !userRole) {
+			return res.status(401).json({ error: "User Id or userRole is missing" });
+		}
+
+		const isMobileClient = req.headers["user-agent"]?.includes("ReactNative");
+
+		const { refreshToken } = await AuthService.refreshCookieToken(userId);
+		const { accessToken } = AuthService.refreshAccessToken(refreshToken);
+
+		if (isMobileClient) {
+			res.json({ accessToken, refreshToken });
+		} else {
+			res.cookie("refreshToken", refreshToken, {
+				httpOnly: true,
+				secure: process.env.NODE_ENV === "production",
+				sameSite: "lax",
+				maxAge: 7 * 24 * 60 * 1000,
+			});
+
+			res.json({ accessToken });
+		}
+	} catch (error: any) {
+		console.error("Refresh token error:", error);
+		return res.status(400).json({ error: error.message || "Invalid token" });
+	}
+};
+
+// token based forget password  controller
+
 export const forgotPassword = async (req: Request, res: Response) => {
 	try {
-		const { phone } = req.body;
+		// const { phone } = req.body;
+		const validated = PasswordSchema.parse(req.body);
+		const phone = standardPhone(validated.phoneNumber);
 		const token = await AuthService.forgotPassword(phone);
 
 		res.json({
@@ -98,9 +160,14 @@ export const forgotPassword = async (req: Request, res: Response) => {
 			token,
 		});
 	} catch (error: any) {
+		if (error instanceof z.ZodError) {
+			return res.status(400).json({ errors: error.errors });
+		}
 		res.status(400).json({ error: error.message });
 	}
 };
+
+// token based reset password controller
 
 export const resetPassword = async (req: Request, res: Response) => {
 	try {
@@ -115,32 +182,39 @@ export const resetPassword = async (req: Request, res: Response) => {
 	}
 };
 
-// export const requestPasswordOtp = async (req: Request, res: Response) => {
-// 	try {
-// 		const { phone } = req.body;
-// 		const { verificationId, expiresIn } =
-// 			await AuthService.requestPasswordResetOtp(phone);
-// 		res.json({ message: "OTP sent", verificationId, expiresIn });
-// 	} catch (error: any) {
-// 		res.status(400).json({ error: error.message });
-// 	}
-// };
+export const requestPasswordOtp = async (req: Request, res: Response) => {
+	try {
+		const validated = PasswordSchema.parse(req.body);
 
-// export const resetPasswordWithOtp = async (req: Request, res: Response) => {
-// 	try {
-// 		const { phone, code, verificationId, newPassword } = req.body;
-// 		await AuthService.resetPasswordWithOtp(
-// 			phone,
-// 			code,
-// 			verificationId,
-// 			newPassword
-// 		);
+		const phone = standardPhone(validated.phoneNumber);
 
-// 		res.json({ message: "Password reset successful" });
-// 	} catch (error: any) {
-// 		res.status(400).json({ error: error.message });
-// 	}
-// };
+		const { verificationId, expiresIn, code } =
+			await AuthService.requestPasswordResetOtp(phone);
+
+		res.json({ message: "OTP sent", verificationId, expiresIn, code });
+	} catch (error: any) {
+		if (error instanceof z.ZodError) {
+			return res.status(400).json({ errors: error.errors });
+		}
+		res.status(400).json({ error: error.message });
+	}
+};
+
+export const resetPasswordWithOtp = async (req: Request, res: Response) => {
+	try {
+		const { phone, code, verificationId, newPassword } = req.body;
+		await AuthService.resetPasswordWithOtp(
+			phone,
+			code,
+			verificationId,
+			newPassword
+		);
+
+		res.json({ message: "Password reset successful" });
+	} catch (error: any) {
+		res.status(400).json({ error: error.message });
+	}
+};
 
 export const getAllUsers = async (req: any, res: any) => {
 	try {
