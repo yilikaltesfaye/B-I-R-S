61a7dc9e5eeb6ed3cb007f2cf6b1ce080897924e
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-10 16:39:40 +0300

    refactor(token): add user role in the jwt payload

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -1,23 +1,30 @@
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
-export const generateAccessToken = (userId: string) => {
-	return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
+
+export const generateAccessToken = (
+	userId: string,
+	userRole: string,
+	refreshToken: string
+) => {
+	return jwt.sign({ userId, userRole }, refreshToken, {
 		expiresIn: "15m",
 	});
 };
-export const generateRefreshToken = (userId: string) => {
-	return jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET!, {
+export const generateRefreshToken = (userId: string, userRole: string) => {
+	return jwt.sign({ userId, userRole }, process.env.REFRESH_TOKEN_SECRET!, {
 		expiresIn: "7d",
 	});
 };
 export const verifyRefreshToken = (token: string) => {
 	return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET!) as {
 		userId: string;
+		userRole: string;
 	};
 };
-export const verifyAccessToken = (token: string) => {
-	return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as {
+export const verifyAccessToken = (token: string, refreshToken: string) => {
+	return jwt.verify(token, refreshToken) as {
 		userId: string;
+		userRole: string;
 	};
 };
 
