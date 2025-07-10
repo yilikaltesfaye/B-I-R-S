9167eee3e9baa5b05c721836d0382ee326475991
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-10 10:07:26 +0300

    feat(auth-token): create functions to generate reset token and to verify access token

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-
+import crypto from "crypto";
 export const generateAccessToken = (userId: string) => {
 	return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
 		expiresIn: "15m",
@@ -14,4 +14,15 @@ export const verifyRefreshToken = (token: string) => {
 	return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET!) as {
 		userId: string;
 	};
+};
+export const verifyAccessToken = (token: string) => {
+	return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as {
+		userId: string;
+	};
+};
+
+export const generateResetToken = () => {
+	const token = crypto.randomBytes(32).toString("hex");
+	const expiry = new Date(Date.now() + 1000 * 60 * 15);
+	return { token, expiry };
 };
