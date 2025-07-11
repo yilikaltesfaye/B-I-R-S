c619deea79616fbded806156c0a5f42eceb48a53
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-11 23:31:25 +0300

    bug-fix: standard phone function makeing every phone error

--- backend/src/utils/standardPhoneNumber.ts
+++ backend/src/utils/standardPhoneNumber.ts
@@ -1,16 +1,12 @@
 export const standardPhone = (input: string): string => {
 	let standard = input.replace(/[^+\d]/g, "");
 
-	if (
-		!standard.startsWith("+2519") ||
-		!standard.startsWith("2519") ||
-		!standard.startsWith("09")
-	) {
-		throw new Error("Incorrect Phone Format");
-	} else if (standard.startsWith("09")) {
+	if (standard.startsWith("09")) {
 		return standard.replace(/^09/, "+2519");
 	} else if (standard.startsWith("251")) {
 		return "+" + standard;
+	} else if (standard.startsWith("9")) {
+		return "+251" + standard;
 	} else if (standard.startsWith("+251")) {
 		return standard;
 	}
