66a4646a1181c1feda8ef3c4cbea4b622a7895e2
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-11 23:06:21 +0300

    feat(zod): updated zod validators for auth routes

--- backend/src/types/auth.interface.ts
+++ backend/src/types/auth.interface.ts
@@ -1,18 +1,36 @@
-import { z } from "zod";
+import z from "zod";
 
-export const SignupSchema = z.object({
+export const RequestOtpSchema = z.object({
+	phoneNumber: z.string().min(9),
+	type: z.string().toUpperCase(),
+});
+
+export const VerifyOtpSchema = z.object({
+	phoneNumber: z.string().min(9),
+	code: z.string().length(6),
+	verificationId: z.string().min(2),
+});
+
+export const RegisterSchema = z.object({
+	guestToken: z.string().length(6),
+	phoneNumber: z.string().min(9),
 	fullName: z.string().min(2),
-	phoneNumber: z.string().min(1),
-	password: z.string().min(8),
-	region: z.string().min(1),
+	password: z.string().min(6),
+	region: z.string().min(2),
 	email: z.string().email().optional(),
 });
 
 export const LoginSchema = z.object({
-	phoneNumber: z.string().min(1),
+	phoneNumber: z.string().min(9),
 	password: z.string().min(6),
 });
 
-export const PasswordSchema = z.object({
-	phoneNumber: z.string().min(1),
+export const checkIfUserExistShema = z.object({
+	phoneNumber: z.string().min(9),
+});
+
+export const PasswordResetSchema = z.object({
+	guestToken: z.string().length(6),
+	phoneNumber: z.string().min(9),
+	newPassword: z.string().min(6),
 });
