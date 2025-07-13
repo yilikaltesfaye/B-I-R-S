53ae2bb4ec25fa615f0df1b095c7e3c9adb2c22e
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-13 15:38:28 +0300

    feat(user service): implement getUserById function to retrieve user details

---
+++ backend/src/modules/user/user.service.ts
@@ -0,0 +1,12 @@
+import prisma from "../../clients/prismaClient";
+
+export const getUserById = (userId: string) => {
+	return prisma.user.findUnique({
+		where: { id: userId },
+		select: {
+			id: true,
+			role: true,
+			refreshToken: true,
+		},
+	});
+};
