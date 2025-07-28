88c463f53fe7aa8ec0127829c059c7f4738cb06c
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-28 09:47:15 +0300

    feat: implement color mode toggle and add Chakra UI components for improved UI experience

--- user-end/src/App.tsx
+++ user-end/src/App.tsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
+import { Button, HStack } from "@chakra-ui/react";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-			<div></div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<HStack>
+				<Button>Click me</Button>
+				<Button>Click me</Button>
+			</HStack>
 		</>
 	);
 }
