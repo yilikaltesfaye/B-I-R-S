bcbcbd43a4fed715bdf11e90fe04d74e7ea0abcc
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-07-31 18:01:34 +0300

    chore: redo admin-end with react router starter template

--- admin-end/vite.config.ts
+++ admin-end/vite.config.ts
@@ -1,14 +1,8 @@
-import path from "path";
+import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
