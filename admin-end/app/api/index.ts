d37b14e946977d7fd2c99819b34824fd372d0adc
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-08-01 11:12:37 +0300

    feat: implement authentication mutations and queries for user registration, login, and logout

---
+++ admin-end/app/api/index.ts
@@ -0,0 +1,12 @@
+export * from "./client";
+export * from "./auth/mutations";
+export * from "./users/queries";
+
+// import { useLogin, useMe } from "@/api/auth/mutations";
+// import { useUserFull } from "@/api/users/queries";
+
+// function LoginForm() {
+//   const { mutate: login } = useLogin();
+//   const { data: user } = useUserFull();
+
+// }
