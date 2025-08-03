5c10dd4c5839b562e9c07f5374278a7b9c7105f2
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-08-03 13:20:17 +0300

    feat: enhance API responses with additional fields for requestOtp, verifyOtp, register, and login

--- user-end/src/api/auth/api.ts
+++ user-end/src/api/auth/api.ts
@@ -7,28 +7,47 @@ import type {
 	ResetPasswordPayload,
 } from "@/types";
 import { apiClient } from "../client";
+enum allowedTypes {
+	"FORGETPASSWORD",
+	"NEWACCOUNT",
+}
 
 export const authApi = {
 	requestOtp: (payload: RequestOtpPayload) =>
-		apiClient.post("/auth/requestotp", payload),
+		apiClient.post<{
+			title: string;
+			message: string;
+			verificationId: string;
+			expiresIn: string;
+			type: allowedTypes;
+		}>("/auth/requestotp", payload),
 	verifyOtp: (payload: VerifyOtpPayload) =>
-		apiClient.post<{ guestToken: string; expiryMin: number }>(
-			"/auth/verifyotp",
-			payload
-		),
+		apiClient.post<{
+			title: string;
+			message: string;
+			guestToken: string;
+			expiryMin: number;
+		}>("/auth/verifyotp", payload),
 	register: (payload: RegisterPayload) =>
-		apiClient.post<{ accessToken: string; userData: User }>(
-			"/auth/register",
-			payload
-		),
+		apiClient.post<{
+			title: string;
+			message: string;
+			accessToken: string;
+			userData: User;
+		}>("/auth/register", payload),
 	login: (payload: LoginPayload) =>
-		apiClient.post<{ accessToken: string; userData: User }>(
-			"/auth/login",
-			payload
-		),
-	logout: () => apiClient.post("/auth/logout"),
+		apiClient.post<{
+			title: string;
+			message: string;
+			accessToken: string;
+			userData: User;
+		}>("/auth/login", payload),
+	logout: () =>
+		apiClient.post<{ title: string; message: string }>("/auth/logout"),
 	refreshAccessToken: () =>
-		apiClient.post<{ accessToken: string }>("/auth/refresh-access-token"),
+		apiClient.post<{ accessToken: string; title: string; message: string }>(
+			"/auth/refresh-access-token"
+		),
 	resetPassword: (payload: ResetPasswordPayload) =>
 		apiClient.post("/resetpassword", payload),
 };
